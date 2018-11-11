c0febf2cc1fc21df9828ebdf9766c9d028c5337d
whimm <whimm@DESKTOP-4VM725I>
2018-11-11 14:21:27 +0800

    add spring-rest-api

---
+++ swagger/swagger-2-documentation-for-spring-rest-api/spring-rest-api/src/main/java/com/ymmihw/libraries/swagger/Application.java
@@ -0,0 +1,11 @@
+package com.ymmihw.libraries.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
