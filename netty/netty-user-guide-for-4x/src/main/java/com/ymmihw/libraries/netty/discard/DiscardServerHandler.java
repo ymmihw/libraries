e492562917446f0f8e8b8e23d5e417bb263ff479
wanghaoming <wanghaoming@ctfo.com>
2021-05-21 13:40:11 +0800

    clean up

--- netty/netty-user-guide-for-4x/src/main/java/com/ymmihw/libraries/netty/discard/DiscardServerHandler.java
+++ netty/netty-user-guide-for-4x/src/main/java/com/ymmihw/libraries/netty/discard/DiscardServerHandler.java
@@ -15,23 +15,24 @@
  */
 package com.ymmihw.libraries.netty.discard;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCountUtil;
 
-/**
- * Handles a server-side channel.
- */
+/** Handles a server-side channel. */
 public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
 
-    @Override
-    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // discard
-    }
+  @Override
+  public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+    ByteBuf in = (ByteBuf) msg;
+    System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+  }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
-        cause.printStackTrace();
-        ctx.close();
-    }
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    // Close the connection when an exception is raised.
+    cause.printStackTrace();
+    ctx.close();
+  }
 }
--- netty/netty-user-guide-for-4x/src/main/java/com/ymmihw/libraries/netty/echo/EchoClientHandler.java
+++ netty/netty-user-guide-for-4x/src/main/java/com/ymmihw/libraries/netty/echo/EchoClientHandler.java
@@ -21,43 +21,40 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
- * Handler implementation for the echo client.  It initiates the ping-pong
- * traffic between the echo client and server by sending the first message to
- * the server.
+ * Handler implementation for the echo client. It initiates the ping-pong traffic between the echo
+ * client and server by sending the first message to the server.
  */
 public class EchoClientHandler extends ChannelInboundHandlerAdapter {
 
-    private final ByteBuf firstMessage;
+  private final ByteBuf firstMessage;
 
-    /**
-     * Creates a client-side handler.
-     */
-    public EchoClientHandler() {
-        firstMessage = Unpooled.buffer(EchoClient.SIZE);
-        for (int i = 0; i < firstMessage.capacity(); i ++) {
-            firstMessage.writeByte((byte) i);
-        }
+  /** Creates a client-side handler. */
+  public EchoClientHandler() {
+    firstMessage = Unpooled.buffer(EchoClient.SIZE);
+    for (int i = 0; i < firstMessage.capacity(); i++) {
+      firstMessage.writeByte((byte) i);
     }
+  }
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(firstMessage);
-    }
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) {
+    ctx.writeAndFlush(firstMessage);
+  }
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ctx.write(msg);
-    }
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ctx.write(msg);
+  }
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-       ctx.flush();
-    }
+  @Override
+  public void channelReadComplete(ChannelHandlerContext ctx) {
+    ctx.flush();
+  }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
-        cause.printStackTrace();
-        ctx.close();
-    }
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    // Close the connection when an exception is raised.
+    cause.printStackTrace();
+    ctx.close();
+  }
 }
