112a1b2149c7e93d2b555d6ce7cf84a4e5b270e3
wanghaoming <wanghaoming@ctfo.com>
2023-05-15 16:34:57 +0800

    add p6spy

---
+++ p6spy/src/main/java/com/ymmihw/SampleP6SpyApplication.java
@@ -0,0 +1,11 @@
+package com.ymmihw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleP6SpyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleP6SpyApplication.class, args);
+    }
+}
